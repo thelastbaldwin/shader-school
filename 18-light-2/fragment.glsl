9395f067e7dc589590b1302eb7984df179c02852
Steve Minor <steve.e.minor@nordstrom.com>
2015-01-07 16:09:52 -0800

    almost have the lambert lighting model figured out

--- 18-light-2/fragment.glsl
+++ 18-light-2/fragment.glsl
@@ -4,6 +4,33 @@ uniform mat4 model, view, projection;
 uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, lightDirection;
 
+varying vec3 vNormal;
+
+float lambertWeight(vec3 n, vec3 d) {
+  return max(dot(n, d), 0.0);
+}
+
+float parallelDistance(
+  vec3 surfacePoint,
+  vec3 surfaceNormal,
+  vec3 p
+) {
+  return dot(p - surfacePoint, surfaceNormal);
+}
+
+vec3 reflectedLight(
+  vec3 normal,
+  vec3 lightDirection,
+  vec3 ambient,
+  vec3 diffuse
+) {
+  float brightness = dot(normal, lightDirection);
+  // float brightness = lambertWeight(normal, lightDirection);
+  return ambient + diffuse * max(brightness, 0.0);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+    vec3 reflectedL = reflectedLight(vNormal, lightDirection, ambient, diffuse);
+    gl_FragColor = vec4(reflectedL, 1.0);
+  // gl_FragColor = vec4(ambient, 1.0);
 }
--- 18-light-2/vertex.glsl
+++ 18-light-2/vertex.glsl
@@ -5,6 +5,9 @@ uniform mat4 model, view, projection;
 uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, lightDirection;
 
+varying vec3 vNormal;
+
 void main() {
-  gl_Position = position;
+  gl_Position = projection * view * model * position;
+  vNormal = normal.xyz;
 }
